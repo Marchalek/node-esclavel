309ba51dedfa70a4d19d0cba3749dd850e6ebf40
Marchalek <marchalek@hotmail.com>
2021-05-29 17:21:55 -0300

    M2 terminado

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const config = require('config')
-const roteador = require('./rotas/fornecedores')
+const express = require('express') //pede o express
+const app = express() //define o app como uma função express
+const config = require('config') //puxa a conifig do arquivo config
+const roteador = require('./rotas/fornecedores') //puxa o rotador 
 
-app.use(express.json())
+app.use(express.json()) //acita o formato json
 
-app.use('/api/fornecedores', roteador)
+app.use('/api/fornecedores', roteador) //
 
 app.listen(config.get('api.porta'), () => console.log('A API esta funcionando!'))
